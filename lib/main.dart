390a18809b9f5e7f69da41f2b35eacc8bd472e6e
dpgehani20 <69641659+dpgehani20@users.noreply.github.com>
2021-01-16 11:13:59 +0530

    splashscreen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/homescreen/homepage.dart';
+import 'package:flutter_application_2/screens/checkingpage.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shimmer/shimmer.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,7 +25,69 @@ class MyApp extends StatelessWidget {
           textTheme: GoogleFonts.poppinsTextTheme(
         Theme.of(context).textTheme,
       )),
-      home: HomePage(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+ @override  
+  void initState() {  
+    super.initState();  
+    Timer(Duration(seconds: 7),  
+            ()=>Navigator.pushReplacement(context,  
+            MaterialPageRoute(builder:  
+                (context) => LoadinPage()  
+            )  
+         )  
+    );  
+  } 
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body:Container(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset('assets/images/bg.png'),
+            // Opacity(
+            //   opacity: 0.5,
+            //   child: Image.asset('assets/bg.png'),
+            // ),
+
+            Shimmer.fromColors(
+              
+                child: Container(
+                  
+                  padding: EdgeInsets.all(16.0),
+                  child: Text('House of Cake',
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    fontFamily: 'Pacifico',
+                    shadows: [
+                      Shadow(
+                        blurRadius: 18.0,
+                        color:Colors.black87,
+                        offset: Offset.fromDirection(120,12)
+                      ),
+                    ]
+                  ),
+                  ),
+                ),
+                baseColor: Color(0xff7f00ff),
+                highlightColor: Color(0xffe100ff)
+            ),
+
+          ],
+        ),
+      ),
     );
   }
 }
